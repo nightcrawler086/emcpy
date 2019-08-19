a57f3f7ab29d13ee941093a155ebbd036a55586d
nightcrawler086 <brian.hall4@gmail.com>
2019-08-19 17:06:05 -0400

    classes!

--- unity/classes.py
+++ unity/classes.py
@@ -1,42 +1,18 @@
+class StorageResource(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
 class Filesystem(object):
-    def __init__(self, name, nasServer, size, poolId, **kwargs):
-        fs = {
-            'name': name,
-            'replicationParameters': {
-                'isReplicationDestination': isReplicationDestination
-            },
-            'snapScheduleParameters': {
-                'snapSchedule': {
-                    'id': snapSchedule
-                }
-            },
-            'fsParameters': {
-                'pool': {
-                    'id': poolId
-                },
-                'nasServer': {
-                    'id': nasServer
-                },
-                'supportedProtocols': supportedProtocols,
-                'flrVersion': flrVersion,
-                'flrMinRetention': flrMinRetention,
-                'flrDefaultRetention': flrDefaultRetention,
-                'flrMaxRetention': flrMaxRetention,
-                'isFlrAutoLockEnabled': isFlrAutoLockEnabled,
-                'isFlrAutoDeleteEnabled': isFlrAutoDeleteEnabled,
-                'flrAutoLockDelay': flrAutoLockDelay,
-                'errorThreshold': errorThreshold,
-                'warningThreshold': warningThreshold,
-                'infoThreshold': infoThreshold,
-                'isThinEnabled': isThinEnabled,
-                'isDataReductionEnabled': isDataReductionEnabled,
-                'isAdvancedDedupEnabled': isAdvancedDedupEnabled,
-                'size': size,
-                'hostIOSize':
-            },
-            'cifsFsParameters': {},
-            'nfsShareCreate': [],
-            'cifsShareCreate': []
-        }
-    def do_nothing(self):
-        pass
+    def __init__(self, name, nasServer, size, pool, **kwargs):
+        self.name = name
+        self.nasServer = nasServer
+        self.size = size
+        self.pool = pool
+        self.__dict__.update(kwargs)
+
+
+class ReplicationParameters(object):
+    def __init__(self, isReplicationDestination):
+        self.isReplicationDestination = isReplicationDestination
+
