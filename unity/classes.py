3ec5f62ed8882223b413bbaf6d1323b5605289ad
nightcrawler086 <brian.hall4@gmail.com>
2019-08-21 23:13:51 -0400

    some useful classes.

--- unity/classes.py
+++ unity/classes.py
@@ -1,24 +1,9 @@
-class pool(object):
-    def __init__(self, id):
-        self.id = id
+import json
 
 
-class nasServer(object):
-    def __init__(self, id):
-        self.id = id
-
-
-class snapSchedule(object):
-    def __init__(self, id):
-        self.id = id
-
-
-class snapScheduleParameters(snapSchedule):
-    pass
-
-
-class fsParameters(pool, nasServer):
+class fsParameters(object):
     def __init__(self, size, **kwargs):
+
         if size[-1] is 'G':
             size_bytes = int(float(size[:-1]) * 1073741824)
         elif size[-1] is 'T':
@@ -26,30 +11,57 @@ class fsParameters(pool, nasServer):
         else:
             print('Size must be specified like 1G (gigabytes) or 1T (terabytes)')
             return
-        size = size_bytes
-        self.__dict__.update(kwargs)
-        super().__init__()
+        self.size = size_bytes
+        self.__dict__.update((k, v) for k, v in kwargs.items() if k in attributes)
+
+
+class pool(object):
+    def __init__(self, poolId):
+        self.id = poolId
+
+
+class nasServer(object):
+    def __init__(self, nasServerId):
+        self.id = nasServerId
 
 
 class replicationParameters(object):
-    def __init__(self, isReplicationDestination):
-        self.isReplicationDestination = isReplicationDestination
-
-
-class cifsParameters(object):
-    def __init__(self, isCIFSSyncWritesEnabled, isCIFSOpLocksEnabled, isCIFSNotifyOnWriteEnabled,
-                 isCIFSNotifyOnAccessEnabled, cifsNotifyOnChangeDirDepth):
-        self.isCIFSSyncWritesEnabled = isCIFSSyncWritesEnabled
-        self.isCIFSOpLocksEnabled = isCIFSOpLocksEnabled
-        self.isCIFSNotifyOnWriteEnabled = isCIFSNotifyOnWriteEnabled
-        self.isCIFSNotifyOnAccessEnabled = isCIFSNotifyOnAccessEnabled
-        self.cifsNotifyOnChangeDirDepth = cifsNotifyOnChangeDirDepth
-
-
-class Filesystem(replicationParameters, snapSceduleParameters, fsParameters, cifsParameters):
-    def __init__(self, name, **kwargs):
-        self.name = name
+    def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+class snapScheduleParameters(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class snapSchedule(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class cifsFileSystemParameters:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+class StorageResourceFilesystem:
+    def __init__(self, name, poolId, nasServerId, size, **kwargs):
+        self.name = name
+        filesystem_parameters = {'supportedProtocols', 'flrVersion', 'flrMinRetention', 'flrDefaultRetention',
+                                'flrMaxRetention', 'isFlrAutoLockEnabled', 'isFlrAutoDeleteEnabled', 'flrAutoLockDelay',
+                                'errorThreshold', 'warningThreshold', 'infoThreshold', 'isThinEnabled',
+                                'isDataReductionEnabled', 'isAdvancedDedupEnabled', 'size', 'hostIOSize',
+                                'sizeAllocated', 'minSizeAllocated', 'isCacheDisabled', 'accessPolicy',
+                                'folderRenamePolicy', 'lockingPolicy', 'poolFullPolicy'}
+        self.fsParameters = fsParameters(size)
+        self.fsParameters.pool = pool(poolId)
+        self.fsParameters.nasServer = nasServer(nasServerId)
+
+    def to_json(self):
+        json_data = json.dumps(self.__dict__, default=lambda o: o.__dict__, indent=4)
+        return json_data
+
+
 
 
 
